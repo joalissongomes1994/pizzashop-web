d989dd06fc36bd1cb42e2cbf1b454dd5dd5249b9
Joalisson Gomes <joalissongomes.webdev@gmail.com>
2024-01-15 08:21:02 -0300

    Project Pizza Shop

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './global.css'
+
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { RouterProvider } from 'react-router-dom'
+import { Toaster } from 'sonner'
+
+import { ThemeProvider } from './components/theme/theme-provider'
+import { router } from './routes'
+
+export function App() {
+  return (
+    <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+      <HelmetProvider>
+        <Toaster richColors />
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={router} />
+      </HelmetProvider>
+    </ThemeProvider>
+  )
+}
